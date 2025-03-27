9537ff2d4c84d8975a53e808c3bcb66957f62e71
ViSiOnOp19cr <chandancr98195@gmail.com>
2025-03-28 00:28:50 +0530

    fixed some bugs

--- src/app/(dashboard)/levels/[levelId]/activities/[activityId]/page.tsx
+++ src/app/(dashboard)/levels/[levelId]/activities/[activityId]/page.tsx
@@ -9,14 +9,9 @@ import QuizActivity from "@/components/activities/QuizActivity";
 import LabActivity from "@/components/activities/LabActivity";
 import ReadingActivity from "@/components/activities/ReadingActivity";
 
-type PageProps = {
-  params: {
-    levelId: string;
-    activityId: string;
-  }
-}
-
-export default async function ActivityPage({ params }: PageProps) {
+export default async function ActivityPage({ params }: { 
+  params: { levelId: string; activityId: string } 
+}) {
   const { userId } = await auth();
   
   if (!userId) {
